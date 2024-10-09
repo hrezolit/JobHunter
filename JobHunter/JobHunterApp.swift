2cc60aa4f1b1f36bb564c1a6feb3fabaa1f84e9f
Nik <hrezolitaz@gmail.com>
2024-10-09 12:39:18 +0600

    Initial Commit

---
+++ JobHunter/JobHunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  JobHunterApp.swift
+//  JobHunter
+//
+//  Created by Nikita on 09.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct JobHunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JobHunterTests/JobHunterTests.swift
@@ -0,0 +1,17 @@
+//
+//  JobHunterTests.swift
+//  JobHunterTests
+//
+//  Created by Nikita on 09.10.2024.
+//
+
+import Testing
+@testable import JobHunter
+
+struct JobHunterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
